a5fbcaeaa09134d8eec36b06fce74fe33054ae19
Gleb Ilinyh <gleb@ilinyh.ru>
2026-08-07 21:52:25 +0500

    debug: add log to start.sh script

--- start.sh
+++ start.sh
@@ -4,17 +4,27 @@ set -eux
 
 CONFIG="${HOME}/.zrok/environment.json"
 
+echo "HOME=$HOME"
+echo "USER=$(id)"
+pwd
+
+ls -la "$HOME"
+ls -la "$HOME/.zrok"
+
 if [ ! -f "$CONFIG" ]; then
     echo "ERROR: ${CONFIG} not found."
     exit 1
 fi
 
-#zrok overview
+echo "=== zrok status ==="
+zrok status || true
 
+echo "=== starting share ==="
+zrok -v share "${ZROK_MODE}" "${ZROK_TARGET}" 2>&1 | tee /tmp/zrok.log
 
-#zrok -v share "${ZROK_MODE}" "${ZROK_TARGET}"
+echo "Exit code: $?"
 
-curl -v https://api-v1.zrok.io/api/v1/
+echo "=== log ==="
+cat /tmp/zrok.log
 
 sleep 6000
-

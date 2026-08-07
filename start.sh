56dd45fe270c7c693808fd03599e35054784962d
Gleb Ilinyh <gleb@ilinyh.ru>
2026-08-08 04:12:08 +0500

    debug: add headless mode

--- start.sh
+++ start.sh
@@ -9,13 +9,4 @@ if [ ! -f "$CONFIG" ]; then
     exit 1
 fi
 
-echo "stdin:"
-test -t 0 && echo "TTY" || echo "NO TTY"
-
-echo "stdout:"
-test -t 1 && echo "TTY" || echo "NO TTY"
-
-echo "stderr:"
-test -t 2 && echo "TTY" || echo "NO TTY"
-
-exec zrok2 share "$ZROK2_MODE" "$ZROK2_TARGET"
+exec zrok2 share --headless "$ZROK2_MODE" "$ZROK2_TARGET"
